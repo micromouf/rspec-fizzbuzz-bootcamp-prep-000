e13da7d70236f71f9f6de507cb73ca44a89e2c7d
Moufoulyroun Kouferidji <micromouf@yahoo.fr>
2019-02-23 16:47:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
-# Don't forget! This file needs to be 'required' in its spec file
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "fizz"
+  end
+end  
+
+
+
+
+
+
+https://learn.co/tracks/bootcamp-prep/ruby-fundamentals/logic-and-conditionals/rspec-fizzbuzz# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
